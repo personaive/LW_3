c5833e815f59c675796963ed29433018a01fac24
personaive <nik.bachurin2704@mail.ru>
2026-03-13 18:05:13 +0500

    Update Program.fs

--- LW_3_2/Program.fs
+++ LW_3_2/Program.fs
@@ -1,9 +1,22 @@
-﻿open System
+open System
+
+let rec readCount () =
+    let input = 
+        Console.ReadLine()
+
+    let success, number = 
+        Int32.TryParse input
+
+    if success && number > 0 then
+        number
+    else
+        printfn "Ошибка. Введите натуральное число:"
+        readCount()
 
 let readLinesSequence (count: int) : seq<string> =
     seq {
         for _ in 1 .. count do
-            yield Console.ReadLine() // считывает строку и добавляет её как следующий элемент
+            yield Console.ReadLine()
     }
 
 let countEven (acc: int) (line: string) =
@@ -13,21 +26,21 @@ let countEven (acc: int) (line: string) =
         acc
 
 let countEvenLength (lines: seq<string>) : int =
-    Seq.fold countEven 0 lines // проходит по каждому элементу и копит результат с помощью acc
+    Seq.fold countEven 0 lines 
 
 [<EntryPoint>]
 let main argv =
 
     printfn "Введите количество строк:"
-    let count = 
-        int (Console.ReadLine())
+    let count =
+        readCount()
 
     printfn "Введите строки:"
 
-    let lines : seq<string> = 
+    let lines : seq<string> =
         readLinesSequence count
 
-    let result = 
+    let result =
         countEvenLength lines
 
     printfn "Количество строк с чётной длиной: %d" result
