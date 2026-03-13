eef06a0adb09f627eaa2e9924dc7e52be8e9ffee
personaive <nik.bachurin2704@mail.ru>
2026-03-13 18:04:42 +0500

    Update Program.fs

--- LW_3_1/Program.fs
+++ LW_3_1/Program.fs
@@ -1,4 +1,30 @@
-﻿open System
+open System
+
+let rec readCount () =
+    let input = 
+        Console.ReadLine()
+
+    let success, number = 
+        Int32.TryParse input
+
+    if success && number >= 0 then
+        number
+    else
+        printfn "Ошибка. Введите натуральное число:"
+        readCount()
+
+let rec readFloat () =
+    let input = 
+        Console.ReadLine()
+
+    let success, number = 
+        Double.TryParse input
+
+    if success then
+        number
+    else
+        printfn "Ошибка. Введите вещественное число:"
+        readFloat()
 
 let truncateNumber (x: float) : int =
     int x
@@ -8,20 +34,24 @@ let main argv =
 
     printfn "Введите количество элементов последовательности:"
     let count =
-        int (Console.ReadLine())
+        readCount()
 
     printfn "Введите вещественные числа:"
+
     let floatSequence =
         seq { for _ in 1 .. count ->
-                float (Console.ReadLine()) }
+                readFloat() }
+        |> Seq.cache
 
+    printfn "Вещественные числа:"
+    floatSequence |> Seq.iter (printf "%.2f ") // %.2f для 
+    printfn ""                                 // чисел float
 
     let intSequence =
         floatSequence |> Seq.map truncateNumber
 
-    printfn "Последовательность без дробной части:"
-
-    intSequence |> Seq.iter (printf "%d ") // проходит по каждому элементу и выполняет действие,
-    printfn ""                             // но не создаёт новую последовательность.
+    printfn "Целые числа (без дробной части):"
+    intSequence |> Seq.iter (printf "%d ")
+    printfn ""
 
     0
